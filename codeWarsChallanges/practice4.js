88477e1f05b6befeb6ba4556e20dbedc95abcf49
we-live-inside-a-dream <rezanaeimabadi7@gmail.com>
2022-03-27 21:15:45 -0600

    returning minimum

---
+++ codeWarsChallanges/practice4.js
@@ -0,0 +1,13 @@
+//Write a function min that takes two arguments and returns their minimum.
+
+function min(x, y){
+    if(x < y) {
+      return x
+    }else{
+      return y
+    }
+  }
+  
+  console.log(min(0, 10));
+
+  console.log(min(0, -10));
